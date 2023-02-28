57a28da6dc50cedc35cc152317e3b9a0fa6673a3
alan <83582613+alannlol@users.noreply.github.com>
2023-02-28 21:09:45 +0000

    Create Services.lua

---
+++ Utility/Services.lua
@@ -0,0 +1,8 @@
+return {
+    ReplicatedStorage = game:GetService("ReplicatedStorage"),
+    UserInputService = game:GetService("UserInputService"),
+    RunService = game:GetService("RunService"),
+    PlayerService = game:GetService("Players"),
+    Workspace = game:GetService("Workspace"),
+    TeamService = game:GetService("Teams"),
+}
